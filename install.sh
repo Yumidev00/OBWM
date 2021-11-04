efc680ca00356786e06fdd8448f1ba86fa52a6db
Yumi <82706323+Yumidev00@users.noreply.github.com>
2021-11-04 22:06:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@
 cp -r polybar/Yumi $HOME/.config/openbox/polybar
 cp -r rofi/Yumi $HOME/.config/openbox/rofi
 cp -r scripts/Yumi.sh $HOME/.config/openbox/scripts
+cp -r picom.conf $HOME/.config/
 cp -r wallpaper/sad.png /usr/share/backgraund
 
 ######################################

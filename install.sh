897da0e851f4a16abdb0e6b9bacd2fd267df926b
Yumi <82706323+Yumidev00@users.noreply.github.com>
2021-12-02 17:20:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,8 @@
 
 cp -r polybar/Yumi $HOME/.config/openbox/polybar
 cp -r rofi/Yumi $HOME/.config/openbox/rofi
+cp -r rofi/bin/themes $HOME/.config/bspwm/rofi/bin
+cp -r rofi/bin/yumi $HOME/.config/bspwm/themes
 cp -r scripts/Yumi.sh $HOME/.config/openbox/scripts
 cp -r picom.conf $HOME/.config/
 cp -r wallpapers $HOME/Pictures
